d620084a6ce25082f27e186687824c2e15a50acd
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-10 18:25:08 +0100

    Create function getting random element from the array

--- app.js
+++ app.js
@@ -6,3 +6,6 @@ const genders = ['M', 'F'];
 const maleNames = ['Antoni', 'Jan', 'Jakub', 'Aleksander', 'Franciszek', 'Szymon', 'Filip', 'Mikołaj', 'Stanisław', 'Wojciech'];
 const femaleNames = ['Julia', 'Zuzanna', 'Zofia', 'Hanna', 'Maja', 'Lena', 'Alicja', 'Oliwia', 'Maria', 'Laura'];
 const lastNames = ['Nowak', 'Wójcik', 'Kowalczyk', 'Woźniak', 'Mazur', 'Krawczyk', 'Kaczmarek', 'Zając', 'Król', 'Wieczorek'];
+
+// Create function getting random element from the array
+const randChoice = (arr) => arr[Math.floor(Math.random() * arr.length)];
