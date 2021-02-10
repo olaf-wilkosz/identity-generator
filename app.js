aa2613545b8b4d8d6efcda0a38edc24aa453f3bf
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-02-10 18:24:14 +0100

    Prepare arrays with data necessary for each identity

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // Import fs module to fs const
 const fs = require('fs');
+
+// Prepare arrays with data necessary for each identity
+const genders = ['M', 'F'];
+const maleNames = ['Antoni', 'Jan', 'Jakub', 'Aleksander', 'Franciszek', 'Szymon', 'Filip', 'Mikołaj', 'Stanisław', 'Wojciech'];
+const femaleNames = ['Julia', 'Zuzanna', 'Zofia', 'Hanna', 'Maja', 'Lena', 'Alicja', 'Oliwia', 'Maria', 'Laura'];
+const lastNames = ['Nowak', 'Wójcik', 'Kowalczyk', 'Woźniak', 'Mazur', 'Krawczyk', 'Kaczmarek', 'Zając', 'Król', 'Wieczorek'];
